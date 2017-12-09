680104888993a1e5c2d852c5c28c34d67c93649c
tangji08 <jojo@JojodeMacBook-Pro.local>
2017-12-09 10:36:39 +0100

    Initial commit

---
+++ src/trees/SpanningTree.java
@@ -0,0 +1,30 @@
+package trees;
+
+import java.util.*;
+
+import graph.*;
+
+public class SpanningTree {
+    
+    public static Collection<Edge> kruskal(UnionFind u, EuclideanGraph g){
+    	// Q2
+    	return null;
+    }
+    
+    public static Collection<Collection<Edge>> kruskal(EuclideanGraph g){
+    	// Q3
+    	return null;
+    }
+    
+    public static Collection<Edge> primTree(HashSet<Place> nonVisited, Place start, EuclideanGraph g){
+    	// Q4
+    	return null;
+    }
+    
+    public static Collection<Collection<Edge>> primForest(EuclideanGraph g){
+    	// Q5
+    	return null;
+    }
+    
+   
+}
---
+++ src/trees/UnionFind.java
@@ -0,0 +1,26 @@
+package trees;
+
+import graph.Place;
+
+import java.util.HashMap;
+
+// Q1
+
+public class UnionFind {
+	//parent relation, parent.put(src,dst) indicates that src points to dst
+    private HashMap<Place,Place> parent;
+    
+    public UnionFind( ){
+        //TODO
+    }
+    
+    public Place find( Place src ){
+        //TODO
+    	return null;
+    }
+    
+    public void union( Place v0, Place v1 ){
+        //TODO
+    	return;
+    }
+}
